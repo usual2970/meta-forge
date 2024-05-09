32ce964b5c78f1c671b979bc2577c69d5bbee1fb
yoan <536464346@qq.com>
2024-05-09 10:40:50 +0800

    详情页设置

--- ui/src/views/EntityDetailView.vue
+++ ui/src/views/EntityDetailView.vue
@@ -15,13 +15,9 @@
   <div class="w-full">
     <a-form>
       <a-row>
-        <a-col :span="12" v-for="(item, index) in formData" :key="index">
-          <a-form-item
-            :label="fieldLabels[index] ? fieldLabels[index] : index"
-            :label-col="{ span: 5 }"
-            :wrapper-col="{ span: 18 }"
-          >
-            <a-input v-model:value="formData[index]" />
+        <a-col :span="12" v-for="(item, index) in fieldLabels" :key="index">
+          <a-form-item :label="item.label" :label-col="{ span: 5 }" :wrapper-col="{ span: 18 }">
+            <a-input v-model:value="formData[item.name]" />
           </a-form-item>
         </a-col>
       </a-row>
@@ -55,23 +51,57 @@ const initFormData = async () => {
     table: router.currentRoute.value.params.name,
     filter: `${idField.name}=${router.currentRoute.value.params.id}`
   })
+
   formData.value = res.data
 }
 
-const fieldLabels = ref({})
+const fieldLabels = ref([])
 
 const initFieldLabels = async (name) => {
   const res = await get({
     key: `${name}_field_label`
   })
+  let data = {}
   if (res.code == 0) {
-    fieldLabels.value = res.data
+    data = res.data
+  }
+  console.log(data, config.value, formData.value)
+  if (config.value) {
+    fieldLabels.value = config.value.fields
+      .filter((item) => {
+        return item.visible
+      })
+      .map((item) => {
+        return {
+          label: data[item.name] ? data[item.name] : item.name,
+          name: item.name
+        }
+      })
+  } else {
+    for (let key in formData.value) {
+      fieldLabels.value.push({
+        label: data[key] ? data[key] : key,
+        name: key
+      })
+    }
   }
 }
 
-onMounted(async () => {
-  await initFormData()
+const config = ref(null)
+const getConfig = async () => {
+  const key = `${router.currentRoute.value.params.name}_crud_detail`
+  const resp = await get({
+    key: key
+  })
+  if (resp.code != 0) {
+    return
+  }
+  config.value = resp.data
+}
 
+onMounted(async () => {
+  await getConfig()
+  await initFormData()
   await initFieldLabels(router.currentRoute.value.params.name)
 })
 </script>

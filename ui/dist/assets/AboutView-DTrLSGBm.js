40f198b8d85634537046cc42090b8d8f3ed86853
yoan <536464346@qq.com>
2024-05-10 16:12:54 +0800

    build

---
+++ ui/dist/assets/AboutView-DTrLSGBm.js
@@ -0,0 +1,1 @@
+import{_ as e,o,e as s,g as t}from"./index-C3yY4eQP.js";const c={},a={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return o(),s("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
---
+++ ui/dist/assets/data-DSng3jim.js
@@ -0,0 +1,1 @@
+import{bX as a}from"./index-C3yY4eQP.js";const e=t=>a({url:"/api/v1/data/list",method:"get",params:t}),s=t=>a({url:"/api/v1/data/detail",method:"get",params:t});export{s as d,e as l};
